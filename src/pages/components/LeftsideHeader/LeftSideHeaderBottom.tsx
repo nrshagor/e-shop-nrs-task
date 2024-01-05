c9dbf35c3022e494ad673dbcc432f74c78accce4
nrshagor <noorerabbishagor@gmail.com>
2024-01-06 01:05:46 +0600

    :pencil: Add user MOdal design Improve

--- src/pages/components/LeftsideHeader/LeftSideHeaderBottom.tsx
+++ src/pages/components/LeftsideHeader/LeftSideHeaderBottom.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import style from "@/styles/leftSideContent.module.scss";
-
+import { MdAdd } from "react-icons/md";
 import { FaUserCircle } from "react-icons/fa";
 import { IoIosAddCircleOutline } from "react-icons/io";
 import Modal from "../Modal/Modal";
@@ -18,11 +18,12 @@ const LeftSideHeaderBottom = () => {
   return (
     <div className={style.leftSideHeaderBottom}>
       <div className={style.headerUserName}>
-        <FaUserCircle /> Steve Jobs
+        <button onClick={openModal}>
+          <FaUserCircle /> Steve Jobs
+        </button>
       </div>
       <div className={style.headerUserAdd}>
         <button onClick={openModal}>
-          {" "}
           <IoIosAddCircleOutline />
         </button>
       </div>
@@ -35,6 +36,10 @@ const LeftSideHeaderBottom = () => {
             <input type="text" placeholder="Currency" />
             <input type="text" placeholder="TAX ID" />
           </div>
+          <div className={style.addMoreDetails}>
+            <MdAdd /> <p>Add More Details</p>
+          </div>
+
           <button type="submit" className={style.update}>
             Update
           </button>
