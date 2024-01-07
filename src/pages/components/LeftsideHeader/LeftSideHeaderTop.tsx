2f8e0ecc7eb46ba47e2a57db8b4920e6f9039e52
nrshagor <noorerabbishagor@gmail.com>
2024-01-07 22:54:14 +0600

    :pencil: settings modal design add

--- src/pages/components/LeftsideHeader/LeftSideHeaderTop.tsx
+++ src/pages/components/LeftsideHeader/LeftSideHeaderTop.tsx
@@ -6,15 +6,20 @@ import { IoSettingsOutline } from "react-icons/io5";
 import { LiaFileInvoiceDollarSolid } from "react-icons/lia";
 import { IoLocationOutline } from "react-icons/io5";
 import { RxCross1 } from "react-icons/rx";
-
+import { FaLessThan } from "react-icons/fa";
 import style from "@/styles/leftSideContent.module.scss";
 import Link from "next/link";
 
 const LeftSideHeaderTop = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
   };
 
   return (
@@ -44,7 +49,35 @@ const LeftSideHeaderTop = () => {
             </div>
             <div className={style.sidebarManu}>
               <IoSettingsOutline />
-              <button> Settings </button>
+              <button onClick={toggleModal}> Settings </button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {/* Modal */}
+      {isModalOpen && (
+        <div className={style.modalOverlay}>
+          <div className={style.modal}>
+            <div className={style.modalHeader}>
+              <button onClick={toggleModal}>
+                <FaLessThan />
+              </button>
+              <h2>Settings</h2>
+            </div>
+            <div className={style.modalBody}>
+              <div className={style.bodayTop}>
+                <h2>Tax On</h2>
+                <h2>Tax Type</h2>
+              </div>
+              <div className={style.bodayBottom}>
+                <div className={style.bodyContent}>
+                  <input type="radio" /> <h2>After Discount</h2>
+                </div>
+                <div className={style.bodyContent}>
+                  <input type="radio" /> <h2>Before Discount</h2>
+                </div>
+              </div>
             </div>
           </div>
         </div>
